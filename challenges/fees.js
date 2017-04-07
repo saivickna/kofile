0dab21ed69c241f946481b8f1d17c18747be6b46
Saivickna Raveendran <saivickna.r@gmail.com>
2017-04-07 13:03:33 -0700

    (feat) Implement mathematical logic for fees challenge

--- challenges/fees.js
+++ challenges/fees.js
@@ -1,1 +1,33 @@
-console.log('Initial test');
+const fees = require('../data/fees.json');
+const orders = require('../data/orders.json');
+
+//Calculate total fees for a given order item
+var calculateOrderItemTotalFee = (type, pages) => {
+  let feeTypeDataList = fees.filter(data => data.order_item_type === type);
+  let total = 0.0;
+  for (let feeTypeData of feeTypeDataList) {
+    for (let fee of feeTypeData.fees) {
+      if (fee.type === 'flat') {
+        total += parseFloat(fee.amount);
+      } else if (fee.type === 'per-page') {
+        total += parseFloat((pages-1)*fee.amount);
+      }
+    }
+  }
+  return total;
+} 
+
+//Calculate total fees for all items in a given order
+var calculateOrderTotalFee = orderItems => {
+  let order = {total: 0.0, itemFees: []};
+  let total = 0.0;
+  for (let item of orderItems) {
+    let itemFee = {type: item.type, price: calculateOrderItemTotalFee(item.type, item.pages)};
+    total += itemFee.price;
+    order.itemFees.push(itemFee);
+  }
+  order.total = total;
+  return order;
+}
+
+console.log(calculateOrderItemTotalFee('Real Property Recording', 5));
