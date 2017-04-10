8e92f063cfe4245077c80aaa8e884c982fd1055c
Saivickna Raveendran <saivickna.r@gmail.com>
2017-04-09 20:06:52 -0700

    (feat) Add initial API functionality

--- server/routeHelpers.js
+++ server/routeHelpers.js
@@ -1,8 +1,27 @@
+const fees = require('../challenges/fees');
+const dist = require('../challenges/distributions');
+
 module.exports = {
   orderPrice: orders => {
-
+    let priceOrders = [];
+    for (let order of orders) {
+      let priceOrder = {
+        order_number: order.order_number, 
+        fees:fees.calculateOrderTotalFee(order.order_items)
+      };
+      priceOrders.append(priceOrder);
+    }
+    return priceOrders;
   },
   orderDist: orders => {
-    
+    let distOrders = [];
+    for (let order of orders) {
+      let distOrder = {
+        order_number: order.order_number, 
+        fees:dist.calculateOrderFundTotal(order.order_items)
+      };
+      distOrders.append(distOrder);
+    }
+    return distOrders;    
   }
 }
