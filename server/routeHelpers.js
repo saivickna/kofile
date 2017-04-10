000f72e4423cd749ff951fd7f5263d347fe9489d
Saivickna Raveendran <saivickna.r@gmail.com>
2017-04-09 20:34:10 -0700

    (fix) Add error handlers and minor fixes

--- server/routeHelpers.js
+++ server/routeHelpers.js
@@ -2,26 +2,39 @@ const fees = require('../lib/fees');
 const dist = require('../lib/distributions');
 
 module.exports = {
-  orderPrice: orders => {
+  orderPrice: (req, res) => {
+    let orders = req.body;
+    if (orders === undefined || !Array.isArray(orders)) res.status(400).send('Order List not provided.');
     let priceOrders = [];
-    for (let order of orders) {
-      let priceOrder = {
-        order_number: order.order_number, 
-        fees:fees.calculateOrderTotalFee(order.order_items)
-      };
-      priceOrders.append(priceOrder);
+    try {
+      for (let order of orders) {
+        let priceOrder = {
+          order_number: order.order_number, 
+          fees:fees.calculateOrderTotalFee(order.order_items)
+        };
+        priceOrders.push(priceOrder);
+      }
+    } catch (e) {
+      console.log(e);
+      res.status(500).send(e);
     }
-    return priceOrders;
+    res.status(200).send(JSON.stringify(priceOrders));
   },
-  orderDist: orders => {
+  orderDist: (req, res) => {
+    let orders = req.body;
+    if (orders === undefined || !Array.isArray(orders)) res.status(400).send('Order List not provided.');
     let distOrders = [];
-    for (let order of orders) {
-      let distOrder = {
-        order_number: order.order_number, 
-        fees:dist.calculateOrderFundTotal(order.order_items)
-      };
-      distOrders.append(distOrder);
+    try {
+      for (let order of orders) {
+        let distOrder = {
+          order_number: order.order_number, 
+          fees:dist.calculateOrderFundTotal(order.order_items)
+        };
+        distOrders.push(distOrder);
+      }
+    } catch (e) {
+      res.status(500).send(e);
     }
-    return distOrders;    
+    res.status(200).send(JSON.stringify(distOrders));   
   }
 }
--- server/routes.js
+++ server/routes.js
@@ -10,5 +10,8 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.post('/orderPrice', helpers.orderPrice);
 app.post('/orderDist', helpers.orderDist);
 
+app.use((req, res) => {
+  res.status(404).send('Invalid endpoint.');
+});
 
 module.exports = app;
